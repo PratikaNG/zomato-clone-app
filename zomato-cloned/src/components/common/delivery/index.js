ea0e6d6cebd9769ce793b81e3bb76a144af24fe8
PratikaNG <pratika.05ng@gmail.com>
2022-12-13 19:39:38 +0530

    filters prepared

--- zomato-cloned/src/components/common/delivery/index.js
+++ zomato-cloned/src/components/common/delivery/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import "./delivery.css";
+import Filters from "../filters";
+
+const deliveryFilters = [
+  {
+    id: 1,
+    icon: <i className="fi fi-rr-settings-sliders absolute-center"></i>,
+    title: Filters,
+  },
+  {
+    id: 2,
+    title: "Rating: 4.0+",
+  },
+  {
+    id: 3,
+    title: "Safe and hygenic",
+  },
+  {
+    id: 4,
+    title: "Pure Veg",
+  },
+  {
+    id: 5,
+    title: "Delivery Time",
+    icon: <i className="fi fi-sr-sort-alt absolute-center"></i>,
+  },
+  {
+    id: 6,
+    title: "Great offers",
+  },
+];
 
 const Delivery = () => {
-  return <div>This is Delivery</div>;
+  return (
+    <div>
+      <div className="max-width">
+        <Filters filterList={deliveryFilters} />
+      </div>
+    </div>
+  );
 };
 
 export default Delivery;
